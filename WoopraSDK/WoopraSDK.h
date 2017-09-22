7e893877db5b049c0be5d84f668fdb042cafa245
Anvar Azizov <anvardejim@gmail.com>
2017-09-22 15:13:22 +0300

    Initial Commit

---
+++ WoopraSDK/WoopraSDK.h
@@ -0,0 +1,19 @@
+//
+//  WoopraSDK.h
+//  WoopraSDK
+//
+//  Created by Anvar Azizov on 22.09.17.
+//  Copyright © 2017 Woopra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WoopraSDK.
+FOUNDATION_EXPORT double WoopraSDKVersionNumber;
+
+//! Project version string for WoopraSDK.
+FOUNDATION_EXPORT const unsigned char WoopraSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WoopraSDK/PublicHeader.h>
+
+
