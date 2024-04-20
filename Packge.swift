8bf097e7a2b205987c4cbbc3bed0fd07d2d7e1c3
jhlin <imjustaup@gmail.com>
2024-04-20 12:01:00 +0800

    build: use swift v5 and align the package name

---
+++ Packge.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "WoopraSDK",
+    products: [
+        .library(
+            name: "WoopraSDK",
+            targets: ["WoopraSDK"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "WoopraSDK",
+            dependencies: [],
+            path: "WoopraSDK"),
+        .testTarget(
+            name: "WoopraSDKTests",
+            dependencies: ["WoopraSDK"],
+            path: "WoopraSDKTests"),
+    ],
+    swiftLanguageVersions: [.v5]    
+)
