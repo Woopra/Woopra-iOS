153908bd1cc9a201e1cb5de31a852d608b08b5ff
jhlin <imjustaup@gmail.com>
2024-04-22 17:18:59 +0800

    build: change product target name

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [
         .library(
             name: "Woopra",
-            targets: ["WoopraSDK"]),
+            targets: ["Woopra"]),
     ],
     dependencies: [],
     targets: [
